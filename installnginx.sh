07ef171e370daa18b14b5c133ef09b33af88308c
b1gb3d <62315118+b1gb3d@users.noreply.github.com>
2020-05-01 20:20:42 -0400

    Update installnginx.sh

--- installnginx.sh
+++ installnginx.sh
@@ -1,9 +1,9 @@
 sudo apt update
 # Install nginx
-sudo apt install nginx
+sudo apt install --assume-yes nginx
 # Install php
-sudo apt install php libapache2-mod-php php-mysql
+sudo apt install --assume-yes php libapache2-mod-php php-mysql
 # Install Additional PHP extensions for WordPress using the command
-sudo apt install php-curl php-gd php-xml php-mbstring  php-xmlrpc php-zip php-soap php-intl
+sudo apt install --assume-yes php-curl php-gd php-xml php-mbstring  php-xmlrpc php-zip php-soap php-intl
 sudo ufw allow in "Nginx Full"
 exit 0
